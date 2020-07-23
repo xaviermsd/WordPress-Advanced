89ace4a51d1b9add57f77364d6311b4269c39ddc
xaviermsd7 <harshprajapati34@gmail.com>
2020-07-23 12:39:03 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+/*Main template file.*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>WordPress Advanced</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
+</html>
